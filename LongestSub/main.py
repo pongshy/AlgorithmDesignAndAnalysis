5411362e39fc37531973386d615808e6dbb2e29c
pongshy <13567172019@163.com>
2020-11-04 17:35:42 +0800

    docs(LongestSub.main.py): 增加了部分注释，以便以后阅读与理解

--- LongestSub/main.py
+++ LongestSub/main.py
@@ -10,8 +10,8 @@ res = list()
 str1 = ""
 str2 = ""
 
+# 使用动态规划算法，获取最长公共子序列长度
 def LCSLength(m, n, x, y, c, b):
-    global store
     for i in range(1, m + 1):
         c[i][0] = 0
     for i in range(1, n + 1):
@@ -20,6 +20,7 @@ def LCSLength(m, n, x, y, c, b):
         for j in range(1, n + 1):
             if x[i] == y[j]:
                 c[i][j] = c[i - 1][j - 1] + 1
+                # 存储str1[i]->str2[j]时，最长公共子序列的类型
                 b[i][j] = 1
             elif c[i - 1][j] >= c[i][j - 1]:
                 c[i][j] = c[i - 1][j]
@@ -38,6 +39,7 @@ def display(i, j, ans):
 
     while i != 0 and j != 0:
         if b[i][j] == 1:
+            # 列表ans临时存储当前最长公共子序列
             ans.insert(0, str1[i])
             i -= 1
             j -= 1
@@ -47,6 +49,8 @@ def display(i, j, ans):
             elif c[i - 1][j] < c[i][j - 1]:
                 j -= 1
             else:
+                # 如果此时c[i - 1][j] = c[i][j - 1]时，无法判断是哪边递推过来的公共子序列
+                # 所以两边都要递归进去
                 display(i - 1, j, copy.deepcopy(ans))
                 display(i, j - 1, copy.deepcopy(ans))
                 return
